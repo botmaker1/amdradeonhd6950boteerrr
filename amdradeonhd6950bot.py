26537ac2326ef69dd7b4f9c8930febe2b5f5dd58
botmaker1 <47451249+botmaker1@users.noreply.github.com>
2020-11-10 22:07:18 +0900

    Update amdradeonhd6950bot.py

--- amdradeonhd6950bot.py
+++ amdradeonhd6950bot.py
@@ -10,7 +10,7 @@ async def on_ready():
     print(client.user.id)
     print("--------------------")
     await client.change_presence(game=discord.Game(name='', type=1))
-
+    await client.change_presence(game=discord.Game(name="도움말 = /A 입력")
 @client.event
 async def on_message(message):
     if message.content.startswith('/A'):
