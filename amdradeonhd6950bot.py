c22f5450ec38c678f6aeb167e01449e675c04e04
botmaker1 <47451249+botmaker1@users.noreply.github.com>
2020-11-11 18:03:14 +0900

    Update amdradeonhd6950bot.py

--- amdradeonhd6950bot.py
+++ amdradeonhd6950bot.py
@@ -11,9 +11,8 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name='', type=1))
 @client.event
 async def on_message(message):
-    if message.author == client.user: # 만약 메시지를 보낸 사람과 봇이 서로 같을 때
-    return
-
+    cmd = message.content.split(prefix)[1].split()[0] # 명령어를 저장한다
+    args = message.content.split(cmd)[1][1:].split() # 공백을 기준으로 args를 저장한다
     if message.content.startswith('za'):
         await client.send_message(message.channel, "AMD Radeon HD 6950 Bot v7.0")  
         await client.send_message(message.channel, "명령어 목록입니다. 모든 명령어 앞에는 z를 입력하세요.")  
