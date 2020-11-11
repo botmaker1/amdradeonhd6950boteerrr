cb3c234b130d18bcb708b01bfe56905edefac411
botmaker1 <47451249+botmaker1@users.noreply.github.com>
2020-11-11 13:55:59 +0900

    Update amdradeonhd6950bot.py

--- amdradeonhd6950bot.py
+++ amdradeonhd6950bot.py
@@ -1,8 +1,7 @@
 import discord
 import os
-from discord.ext import commands
 client = discord.Client()
-app = commands.Bot(command_prefix='z')
+
 @client.event
 async def on_ready():
     print("Login")
@@ -11,9 +10,6 @@ async def on_ready():
     print("--------------------")
     await client.change_presence(game=discord.Game(name='', type=1))
 @client.event
-@app.command()
-async def hello(ctx):
-    await ctx.send('Hello, World!')
 async def on_message(message):
     if message.content.startswith('za'):
         await client.send_message(message.channel, "AMD Radeon HD 6950 Bot v7.0")  
